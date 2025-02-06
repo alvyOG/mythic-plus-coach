26ba8353011b5f875b0ea25cd41c40724f66cb80
Alex <aralvarez023@gmail.com>
2025-02-06 12:20:54 -0500

    Fix tabs in CoreSettings.lua

--- MythicPlusAnalyzer/CoreSettings.lua
+++ MythicPlusAnalyzer/CoreSettings.lua
@@ -7,7 +7,7 @@ CoreSettings.trackingTabsOrder = {}
 
 -- Show the Settings Window
 function CoreSettings:Show()
-    if not settingsWindow then
+    if not self.settingsWindow then
         self:CreateSettingsWindow()
     end
     self.settingsWindow:Show()
@@ -27,49 +27,62 @@ function CoreSettings:CreateSettingsWindow()
     self.settingsWindow:SetLayout("Flow")
     self.settingsWindow:SetWidth(400)
     self.settingsWindow:SetHeight(300)
-    
+
+    -- Create Tabs Group
     local settingsTabs = AceGUI:Create("TabGroup")
     settingsTabs:SetLayout("Flow")
-    settingsTabs:SetFullWidth("True")
+    settingsTabs:SetFullWidth(true)
     settingsTabs:SetTabs({
         { text = "General", value = "General" },
         { text = "Tracking", value = "Tracking" }
     })
+
+    -- Create a container for tab content
+    local tabContainer = AceGUI:Create("SimpleGroup")
+    tabContainer:SetFullWidth(true)
+    tabContainer:SetFullHeight(true)
+    tabContainer:SetLayout("Flow")
+
+    -- Handle tab selection
+    settingsTabs:SetCallback("OnGroupSelected", function(container, event, group)
+        tabContainer:ReleaseChildren() -- Clear previous tab content
+        if group == "General" then
+            CoreSettings:CreateGeneralTab(tabContainer)
+        elseif group == "Tracking" then
+            CoreSettings:CreateTrackingTab(tabContainer)
+        end
+    end)
+
+    -- Add components in the correct order
     self.settingsWindow:AddChild(settingsTabs)
-    
-    local generalContainer = AceGUI:Create("SimpleGroup")
-    generalContainer:SetFullWidth(true)
-    generalContainer:SetLayout("Flow")
-    settingsTabs:AddChild(generalContainer)
-    
-    local trackingContainer = AceGUI:Create("SimpleGroup")
-    trackingContainer:SetFullWidth(true)
-    trackingContainer:SetLayout("Flow")
-    settingsTabs:AddChild(trackingContainer)
-    
-    self:CreateTrackingTab(trackingContainer)
+    self.settingsWindow:AddChild(tabContainer)
+
+    -- Set default tab
     settingsTabs:SelectTab("General")
+end
+
+-- Create the General Tab
+function CoreSettings:CreateGeneralTab(container)
+    local label = AceGUI:Create("Label")
+    label:SetText("General Settings go here.")
+    label:SetFullWidth(true)
+    container:AddChild(label)
 end
 
 -- Create the Tracking Tab
 function CoreSettings:CreateTrackingTab(container)
-    container:ReleaseChildren()
     local plugins = MythicPlusAnalyzer.plugins
-    if #self.trackingTabsOrder  == 0 then
+    if #self.trackingTabsOrder == 0 then
         for _, plugin in ipairs(plugins) do
-            table.insert(self.trackingTabsOrder, {text = plugin.name, value = plugin.name})
+            table.insert(self.trackingTabsOrder, { text = plugin.name, value = plugin.name })
         end
     end
-    
-    for index, tab in ipairs(self.trackingTabsOrder) do
-        local group = AceGUI:Create("SimpleGroup")
-        group:SetFullWidth(true)
-        group:SetLayout("Flow")
 
+    for index, tab in ipairs(self.trackingTabsOrder) do
         local pluginLabel = AceGUI:Create("Label")
         pluginLabel:SetText(tab.value)
         pluginLabel:SetWidth(120)
-        group:AddChild(pluginLabel)
+        container:AddChild(pluginLabel)
 
         local enableButton = AceGUI:Create("Button")
         enableButton:SetText("Enable")
@@ -77,7 +90,7 @@ function CoreSettings:CreateTrackingTab(container)
         enableButton:SetCallback("OnClick", function()
             MythicPlusAnalyzer:SetPluginEnable(tab.value, true)
         end)
-        group:AddChild(enableButton)
+        container:AddChild(enableButton)
 
         local disableButton = AceGUI:Create("Button")
         disableButton:SetText("Disable")
@@ -85,33 +98,29 @@ function CoreSettings:CreateTrackingTab(container)
         disableButton:SetCallback("OnClick", function()
             MythicPlusAnalyzer:SetPluginEnable(tab.value, false)
         end)
-        group:AddChild(disableButton)
+        container:AddChild(disableButton)
 
         local upButton = AceGUI:Create("Button")
-        upButton:SetText("↑")
+        upButton:SetText("+")
         upButton:SetWidth(30)
         upButton:SetCallback("OnClick", function()
             if index > 1 then
                 self.trackingTabsOrder[index], self.trackingTabsOrder[index - 1] = self.trackingTabsOrder[index - 1], self.trackingTabsOrder[index]
-                CoreWindow:UpdateTabs(self.trackingTabsOrder)
                 self:CreateTrackingTab(container)
             end
         end)
-        group:AddChild(upButton)
+        container:AddChild(upButton)
 
         local downButton = AceGUI:Create("Button")
-        downButton:SetText("↓")
+        downButton:SetText("-")
         downButton:SetWidth(30)
         downButton:SetCallback("OnClick", function()
             if index < #self.trackingTabsOrder then
                 self.trackingTabsOrder[index], self.trackingTabsOrder[index + 1] = self.trackingTabsOrder[index + 1], self.trackingTabsOrder[index]
-                CoreWindow:UpdateTabs(self.trackingTabsOrder)
                 self:CreateTrackingTab(container)
             end
         end)
-        group:AddChild(downButton)
-
-        container:AddChild(group)
+        container:AddChild(downButton)
     end
 end
 
